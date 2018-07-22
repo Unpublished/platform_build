cbb158c86c43855ebe196b5b7ffb6a2ef10e3fcb
Unpublished <Unpublished@users.noreply.github.com>
2018-07-22 15:55:53 +0200

    Update main.mk

--- core/main.mk
+++ core/main.mk
@@ -211,12 +211,7 @@ $(error stop)
 endif
 
 # Check for the current JDK.
-#
-# For Java 1.7/1.8, we require OpenJDK on linux and Oracle JDK on Mac OS.
 requires_openjdk := false
-ifeq ($(BUILD_OS),linux)
-requires_openjdk := true
-endif
 
 
 # Check for the current jdk
